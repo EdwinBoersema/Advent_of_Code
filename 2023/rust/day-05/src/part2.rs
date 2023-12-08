99947f04bc6a663337d89473baeb81adb9f4b54a
Edwin Boersema <edwinboersema@gmail.com>
2023-12-08 15:51:15 +0100

    Reverted multithreading changes.

--- 2023/rust/day-05/src/part2.rs
+++ 2023/rust/day-05/src/part2.rs
@@ -1,5 +1,28 @@
-pub fn process(_input: &str) -> String {
-    todo!("Solve part 2 of the problem");
+use crate::utility::{Id, get_seed_numbers, get_mappings, get_min_location_id};
+
+pub fn process(input: &str) -> Id {
+    let mut input_lines = input.lines();
+    let seeds = get_seed_numbers(&mut input_lines);
+    let mappings = get_mappings(&mut input_lines);
+
+    // convert seeds to ranges of seeds
+    let mut seed_ranges = Vec::new();
+    let mut start: Id = 0;
+    // get 1st number, add Id's for the range length
+    for (i, &seed) in seeds.iter().enumerate() {
+        if i%2 == 0 {
+            start = seed;
+        } else {
+            // push seed range into the vector
+            seed_ranges.push(start..(start + seed));
+        }
+    }
+
+    seed_ranges
+        .into_iter()
+        .map(|r| get_min_location_id(r.into_iter(), &mappings))
+        .min()
+        .expect("Cannot find a minimum id from the seed ranges")
 }
 
 #[cfg(test)]
@@ -8,8 +31,7 @@ mod test {
 
     #[test]
     fn test_process() {
-        todo!("Haven't built the test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = include_str!("../test.txt");
+        assert_eq!(46, process(input));
     }
 }
